0ef2f783e902141e9263ce5de6b4d85314a48a94
Adrian Ancona Novelo <soonick5@yahoo.com.mx>
2015-04-17 16:49:04 +0200

    Use a canvas for main image container

--- src/main.js
+++ src/main.js
@@ -9,18 +9,19 @@
  * @param {object} options - Configuration options
  */
 var ImageShaver = function(container, options) {
-  options = options || {};
+  this.options = options || {};
 
   this.container = container;
   this.createDom();
-  this.showOriginalImage(options.image);
+  this.showOriginalImage();
 };
 
 /**
  * Destroys the contents of the shaver container and creates it again
  */
 ImageShaver.prototype.createDom = function() {
-  this.original = document.createElement('DIV');
+  this.original = document.createElement('CANVAS');
+  this.originalCtx = this.original.getContext('2d');
   this.preview = document.createElement('DIV');
   this.original.className = 'shaver-original';
   this.preview.className = 'shaver-preview';
@@ -28,16 +29,33 @@ ImageShaver.prototype.createDom = function() {
   this.container.innerHTML = '';
   this.container.appendChild(this.original);
   this.container.appendChild(this.preview);
+
+  // For canvas to correctly resize its contents you have to use width and
+  // height attributes to specify its height
+  this.original.setAttribute('height', this.original.offsetHeight);
+  this.original.setAttribute('width', this.original.offsetWidth);
 };
 
 /**
  * Shows the given image in the original container
- * @param {string} url - image url
  */
-ImageShaver.prototype.showOriginalImage = function(url) {
-  if (!url) {
+ImageShaver.prototype.showOriginalImage = function() {
+  if (!this.options.image) {
     return;
   }
 
-  this.original.innerHTML = '<img src="' + url + '">';
+  var image = document.createElement('IMG');
+  image.setAttribute('src', this.options.image);
+  image.className = 'shaver-hidden-image';
+  this.container.appendChild(image);
+  this.hiddenImage = image;
+  this.drawHiddenImage();
+};
+
+/**
+ * Draws hiddenImage on canvas
+ */
+ImageShaver.prototype.drawHiddenImage = function() {
+  this.originalCtx.drawImage(this.hiddenImage, 0, 0, this.original.offsetWidth,
+      this.original.offsetHeight);
 };
--- tests/unit/src/main.spec.js
+++ tests/unit/src/main.spec.js
@@ -14,9 +14,12 @@ describe('imageShaver', function() {
     it('destroys the contents of the container and creates shaver and preview', function() {
       this.instance.createDom();
 
-      var expected = '<div class="shaver-original"></div>' +
-          '<div class="shaver-preview"></div>';
-      proclaim.equal(expected, this.instance.container.innerHTML);
+      var children = this.instance.container.children;
+      proclaim.equal(2, children.length);
+      proclaim.equal('CANVAS', children[0].tagName);
+      proclaim.equal('shaver-original', children[0].className);
+      proclaim.equal('DIV', children[1].tagName);
+      proclaim.equal('shaver-preview', children[1].className);
     });
 
     it('saves reference to original and preview', function() {
@@ -24,26 +27,53 @@ describe('imageShaver', function() {
 
       var c = this.instance.container;
       var expectedOriginal = c.getElementsByClassName('shaver-original')[0];
+      var expectedOriginalCtx = expectedOriginal.getContext('2d');
       var expectedPreview = c.getElementsByClassName('shaver-preview')[0];
 
       proclaim.equal(expectedOriginal, this.instance.original);
+      proclaim.equal(expectedOriginalCtx, this.instance.originalCtx);
       proclaim.equal(expectedPreview, this.instance.preview);
+    });
+
+    it('sets canvas width and height attributes', function() {
+      var canvasSize = document.createElement('STYLE');
+      canvasSize.innerHTML = 'canvas { width: 201px; height: 200px}';
+      document.body.appendChild(canvasSize);
+
+      this.instance.createDom();
+
+      proclaim.equal(200, this.instance.original.getAttribute('height'));
+      proclaim.equal(201, this.instance.original.getAttribute('width'));
     });
   });
 
   describe('showOriginalImage', function() {
-    it('shows given image inside original container', function() {
-      var url = '/base/tests/unit/fixtures/img/img.png';
-      this.instance.showOriginalImage(url);
-
-      var expected = '<img src="' + url + '">';
-      proclaim.equal(expected, this.instance.original.innerHTML);
-    });
-
     it('does not try to load image if no url was given', function() {
+      delete this.instance.options.image;
       this.instance.showOriginalImage();
 
-      proclaim.equal('', this.instance.original.innerHTML);
+      var hiddenImage =
+          this.instance.container.getElementsByClassName('shaver-hidden-image');
+      proclaim.equal(0, hiddenImage.length);
+    });
+
+    it('adds hidden image to container', function() {
+      this.instance.options.image = '/base/tests/unit/fixtures/img/img.png';
+      this.instance.showOriginalImage();
+
+      var hiddenImage =
+          this.instance.container.getElementsByClassName('shaver-hidden-image');
+      proclaim.equal(1, hiddenImage.length);
+    });
+
+    it('hidden image has correct URL', function() {
+      this.instance.options.image = '/base/tests/unit/fixtures/img/img.png';
+      this.instance.showOriginalImage();
+
+      proclaim.equal(
+        this.instance.options.image,
+        this.instance.hiddenImage.getAttribute('src')
+      );
     });
   });
 });
